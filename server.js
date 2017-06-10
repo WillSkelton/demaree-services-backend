5ca2d237c0917f7cda433fd7d6a458180078b23c
william-skelton <william.f.skelton@outlook.com>
2017-06-10 16:35:37 -0700

    added models and also error-middleware.js

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// Node Modules
+const express = require('express');
+const debug = require('debug')('demaree-services:server');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Routes
+
+
+dotenv.load();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const morganFormat = process.env.PRODUCTION ? 'common' : 'dev';
+
+app.use(cors());
+app.use(morgan(morganFormat));
+
+app.get('/', (req, res) => {
+  res.send('Test McTesterson');
+});
+
+app.listen(PORT, () => {
+  debug(`Port up: ${PORT}`);
+});
